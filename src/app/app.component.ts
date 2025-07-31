5c891bf739374bce5910ae48b215a2a45c43b0e7
IanGregoire <ilghbr@gmail.com>
2025-07-31 16:47:52 -0400

    Feat: Add light/dark toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  isDarkMode = false;
 
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+
+    const root = document.documentElement;
+    if(this.isDarkMode) {
+      root.classList.add('dark');
+    } else {
+      root.classList.remove('dark');
+    }
+
+    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+  }
+
+  ngOnInit() {
+    const savedTheme = localStorage.getItem('theme');
+    this.isDarkMode = savedTheme === 'dark';
+    if(this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+    }
+  }
 }
