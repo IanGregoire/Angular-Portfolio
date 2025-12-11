6de23398f0ff222bf62c9d76013d2ec228fcf005
IanGregoire <ilghbr@gmail.com>
2025-12-10 22:45:33 -0500

    Feat: App component and Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet, CommonModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   isDarkMode = false;
+  currentYear = 2025;
 
   toggleTheme() {
     this.isDarkMode = !this.isDarkMode;
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'technology', component: TechnologyComponent }
+];
+
+// export const routes: Routes = [
+//   {
+//     path: '',
+//     redirectTo: 'home',
+//     pathMatch: 'full',
+//   },
+//   {
+//     path: 'home',
+//     loadComponent: () =>
+//       import('./pages/home/home.component').then(m => m.HomeComponent),
+//   },
+//   {
+//     path: 'about',
+//     loadComponent: () =>
+//       import('./pages/about/about.component').then(m => m.AboutComponent),
+//   },
+//   {
+//     path: 'technology',
+//     loadComponent: () =>
+//       import('./pages/technology/technology.component').then(m => m.TechnologyComponent),
+//   },
+//   {
+//     path: 'projects',
+//     loadComponent: () =>
+//       import('./pages/projects/projects.component').then(m => m.ProjectsComponent),
+//   },
+//   {
+//     path: '**',
+//     redirectTo: 'home',
+//   },
+// ];
